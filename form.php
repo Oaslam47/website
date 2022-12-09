1373bd85b2e4fb2e192346376009c81711bf2b86
Oaslam47 <116277825+Oaslam47@users.noreply.github.com>
2022-12-09 12:46:28 +0000

    Add files via upload

---
+++ form.php
@@ -0,0 +1,30 @@
+<?php
+
+	$hostName = "localhost";
+	$userName = "root";
+	$passWord = "";
+	$dbName = "users";
+	// Create connection
+	$conn = mysqli_connect($hostName, $userName, $passWord, $dbName);
+
+	// Check connection
+	if (!$conn) {
+    	die("Connection failed: " . mysqli_connect_error());
+	}
+	else{
+		echo 'Connected successfully';
+	}
+
+	$fname = $_POST['fname'];
+	$email = $_POST['email'];
+	$msg = $_POST['msg'];
+
+	$sql = "INSERT INTO entries(fname,email,msg) VALUES ('$fname','$email', '$msg')";
+	if(mysqli_query($conn,$sql)){
+		echo "done";
+	}
+
+	// Close connection
+	mysqli_close($conn);
+	
+?>	
